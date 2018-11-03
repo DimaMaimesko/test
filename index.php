1bab5ac367d0316c2fc10e70d4c5e2cde15ec6c9
Dima Maimesko <dima.maimesko@gmail.com>
2018-11-03 03:09:28 +0200

    Deleted

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-echo 'Hello World!';
-//делаем из страницы сайта свежайший "клон" Яндекса
- //$str = file_get_contents ('http://parsing.valemak.com/ru/php/import/file-get-contents/');
- //
-
-$text = file_get_contents( 'http://mentor.su/prostoy-parser-na-php/' );
-$newText = preg_replace( '/strong/' ,"i", $text); 
-    
-    echo $newText;
+/
